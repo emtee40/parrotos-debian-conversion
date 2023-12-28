0a175df34bae18c820691bd368602889b38694fd
danterolle <danterolle@parrotsec.org>
2023-12-28 13:54:44 +0100

    add run and handle_error function

--- installer.sh
+++ installer.sh
@@ -8,10 +8,50 @@ function check_sudo() {
     fi
 }
 
-#### Main functions ####
+# Prints an error message and exits the script
+function handle_error() {
+    local error_message="$1"
+    echo "Error: $error_message"
+    exit 1
+}
 
+# Executes a command and handles errors
+function run() {
+    local command="$1"
+    local description="$2"
+
+    echo "Executing: $description"
+
+    eval "$command"
+    local exit_code=$?
+
+    if [ $exit_code -ne 0 ]; then
+        handle_error "Command failed: $description"
+    fi
+}
+
+# Main functions
+
+# Installs the Parrot OS Core packages.
 function core() {
+    run "apt update" "updating package lists"
+    run "apt install -y bash wget gnupg" "installing required packages"
 
+    run "wget -qO- https://deb.parrotsec.org/parrot/misc/parrotsec.gpg | apt-key add -" "adding GPG key"
+
+    run "cp /etc/apt/sources.list /etc/apt/sources.list" "copying sources.list"
+    run "cp -r /etc/apt/sources.list.d/* /etc/apt/sources.list.d" "copying sources.list.d"
+    run "cp /etc/apt/listchanges.conf /etc/apt/listchanges.conf" "copying listchanges.conf"
+
+    run "apt update" "updating package lists"
+
+    run "cp /etc/os-release /etc/os-release" "copying os-release"
+    run "apt update" "updating package lists"
+    run "apt upgrade -y" "upgrading packages"
+
+    run "apt install -y parrot-core" "installing parrot-core"
+
+    echo "ParrotOS Core installation completed successfully."
 }
 
 function home() {
